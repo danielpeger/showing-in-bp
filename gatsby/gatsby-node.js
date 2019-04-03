7db4fab068a6b9b9449c0cb0f8baf6f513069040
danielpeger <pegerdaniel@gmail.com>
2019-04-03 18:07:58 +0200

    scrape screenings

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -2,28 +2,45 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch();
-  const listPage = await browser.newPage();
 
-  await listPage.goto('http://est.hu/mozi/filmek_a_heten/#varos=298');
-
-  const result = await listPage.evaluate(() => {
+  const page = await browser.newPage();
+  await page.goto('http://est.hu/mozi/filmek_a_heten/#varos=298');
+  const listResult = await page.evaluate(() => {
 		let res = [];
 		const egyseg = document.querySelectorAll('.egyseg');
 		for (let i = 0; i < egyseg.length; i++) {
 			let film = {};
 			let titleAndYear = egyseg[i].querySelector('small').textContent;
-			film.hungarianTitle = egyseg[i].querySelector('a').textContent;
+			let link = egyseg[i].querySelector(':scope > a');
+			film.hungarianTitle = link.textContent;
 			if (titleAndYear.includes(',')) {
 				film.title = titleAndYear.substring(1, titleAndYear.indexOf(','));
 			} else {
 				film.title = film.hungarianTitle;
 			};
+			let estUrl = link.getAttribute('href');
+			film.estId = estUrl.substring(estUrl.indexOf('=')+1, estUrl.length);
 			res.push(film);
 		}
 		return res;
   });
 
-	console.log(result);
+	for (let film of listResult) {
+		await page.goto('http://est.hu/mozi/filmek_a_heten/#filmkereso/film=' + film.estId);
+		const screeningResult = await page.evaluate(() => {
+			let res = [];
+			const focella = document.querySelectorAll('.fo_cella a');
+			for (let i = 0; i < focella.length; i++) {
+				let screening = {};
+				screening.location = focella[i].textContent;
+				res.push(screening);
+			}
+			return res;
+		})
+		film.screening = screeningResult;
+	}
+
+	console.log(JSON.stringify(listResult, null, 2));
 
   browser.close();
 })();
