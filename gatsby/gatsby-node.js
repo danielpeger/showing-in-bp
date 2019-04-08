6cf19a5ae1f36c06aa8ebc91c2a4d06639df280a
danielpeger <pegerdaniel@gmail.com>
2019-04-08 22:13:16 +0200

    -

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const axios = require("axios");
 
 async function getData() {
   const browser = await puppeteer.launch();
@@ -15,11 +16,27 @@ async function getData() {
       film.hungarianTitle = link.textContent;
       if (titleAndYear.includes(",")) {
         film.title = titleAndYear.substring(1, titleAndYear.indexOf(","));
+				const year = titleAndYear.substring(titleAndYear.indexOf(",")+2, titleAndYear.length-1);
+				film.year = parseInt(year);
       } else {
         film.title = film.hungarianTitle;
+				const year = titleAndYear.substring(1, titleAndYear.length-1);
+				film.year = parseInt(year);
       }
       let estUrl = link.getAttribute("href");
       film.estId = estUrl.substring(estUrl.indexOf("=") + 1, estUrl.length);
+
+			const tmdbApiKey = '4b16b5a9a0f5ff8dcbe7170c12fca839';
+			const tmdbUrl = 'https://api.themoviedb.org/3/search/movie?api_key='+tmdbApiKey+'&language=en-US&query='+film.title+'&year='+film.year;
+			axios.get(tmdbUrl)
+			  .then(function (response) {
+					film.desription = response.results[0].overview;
+			  })
+			  .catch(function (error) {
+			    // handle error
+			    console.log(error);
+			  });
+
       res.push(film);
     }
     return res;
@@ -42,9 +59,36 @@ async function getData() {
         let screening = {};
         screening.location = row[i].querySelector(".fo_cella a").textContent;
 				const cells = row[i].querySelectorAll("td > div");
-				screening.times = [];
+				screening.events = [];
 				for (var j = 0; j < cells.length; j++) {
-					screening.time.push(cells[j].textContent);
+					let event = {};
+					let cellText = cells[j].textContent;
+
+					let hours = cellText.substring(0, cellText.indexOf(':'));
+					let minutes = cellText.substring(cellText.indexOf(':')+1, cellText.indexOf('('));
+					let eventDate = new Date();
+					eventDate.setHours(hours);
+					eventDate.setMinutes(minutes);
+					event.time = eventDate.toISOString();
+
+					let dubString = cellText.substring(cellText.indexOf('(')+1, cellText.length-1)
+					if (dubString = 'mb') {
+						event.dubbed = true;
+						event.subtitled = false;
+						event.subtitleLanguage = false;
+					} else if (dubString = 'f') {
+						event.dubbed = false;
+						event.subtitled = true;
+						event.subtitleLanguage = 'hungarian';
+					} else if (dubString = 'ensub') {
+						event.dubbed = false;
+						event.subtitled = true;
+						event.subtitleLanguage = 'english';
+					} else {
+						event.dubbed = dubString;
+					}
+
+					screening.events.push(event);
 				}
         res.push(screening);
       }
@@ -53,14 +97,13 @@ async function getData() {
     film.screenings = screeningResult;
   }
 
-  console.log(JSON.stringify(listResult, null, 2));
+	console.log(JSON.stringify(listResult, null, 2));
 
   browser.close();
 
   return listResult;
 }
 
-//document.querySelectorAll('.talalat_tabla td div')
 getData();
 
 exports.createPages = async ({ actions: { createPage } }) => {
